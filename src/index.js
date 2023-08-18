6986551b1a38312b84ee586c9499bbe5635e09f6
Kumari Riya <riyachouhan.ee@gmail.com>
2023-08-18 18:23:08 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import mongoose from "mongoose";
 import { userRouter } from "./routes/users.js";
 import { recipesRouter } from "./routes/recipes.js";
 
-
 const app = express();
+const port = process.env.PORT || 5001; // Default to port 5001 if PORT is not set
+
+// Define your MongoDB connection URI directly here
+const mongoDBURI = "mongodb+srv://riya:riya123@recipeapp.eijjtbh.mongodb.net/recipeApp?retryWrites=true&w=majority";
 
 app.use(express.json());
 app.use(cors());
@@ -14,11 +17,11 @@ app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
 
 mongoose.connect(
-  "mongodb+srv://riya:riya123@recipeapp.eijjtbh.mongodb.net/recipeApp?retryWrites=true&w=majority",
+  mongoDBURI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
 
-app.listen(5001, () => console.log("Server started"));
+app.listen(port, () => console.log(`Server started on port ${port}`));
